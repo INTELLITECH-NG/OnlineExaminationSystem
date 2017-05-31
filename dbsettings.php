982e78eda5fc256ad727e16bc1e311a3a485e446
Akhil Kumar <akhilkumar332@gmail.com>
2017-06-01 02:26:05 +0530

    Add files via upload

---
+++ dbsettings.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+***************************************************
+*** TechWell Online Examination System          ***
+***---------------------------------------------***
+*** License: GNU General Public License V.3     ***
+*** Author: Akhil Kumar                         ***
+*** Title: Database Settings                    ***
+***************************************************
+*/
+
+//This is the name of your server where the MySQL database is running
+$dbserver="localhost";
+
+//username of the MySQL server
+$dbusername="dbuser";
+
+//password
+
+$dbpassword="dbpass";
+
+//database name of the online Examination system
+$dbname="id921602_toes";
+
+?>
